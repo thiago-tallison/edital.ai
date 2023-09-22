c6adc14051fab3b1bc29a4060b63b53ff6d8254e
Thiago Tallison <thiagotallison@gmail.com>
2023-09-22 19:07:30 -0300

    refactor: change how-to-use page content

--- src/app/how-to-use/page.tsx
+++ src/app/how-to-use/page.tsx
@@ -1,40 +1,39 @@
 export default function HowToUse() {
   return (
-    <>
-      <h1 className='text-2xl'>Como usar</h1>
+    <div>
+      <h1 className='mb-6 text-2xl'>Como usar</h1>
 
-      <ol>
-        <li>
-          Copie todas as disciplinas para o cargo desejado do edital e cole-as
-          no campo <strong>DISCIPLINAS DO EDITAL</strong>;
+      <ol className='list-inside space-y-6'>
+        <li className='list-decimal'>
+          Copie todas as disciplinas para o cargo desejado e cole-as no campo{' '}
+          <strong>Disciplinas do Edital</strong>
           <ul>
-            <li>
-              Obs: caso a seleção inclua o número da página do documento,
-              considere excluí-la antes de processar o texto. Este comportamento
-              é comum em documentos do tipo PDF.
+            <li className='ml-6 mt-2'>
+              <strong>Obs:</strong> caso a seleção inclua o número da página do
+              documento, considere excluí-la antes de processar o texto.
             </li>
           </ul>
         </li>
 
-        <li>
-          Configurações;
+        <li className='list-decimal'>
+          Configurações
           <ul>
-            <li>
-              <strong>Sem numeração:</strong> marque esta caixa se não deseja
+            <li className='ml-6 mt-2'>
+              <strong>Sem numeração:</strong> marque esta caixa caso não deseje
               numeração em cada tópico.
             </li>
-            <li>
-              <strong>Espaçamento:</strong> a quantidade de espaços para identar
+            <li className='ml-6 mt-2'>
+              <strong>Espaçamento:</strong> a quantidade de espaços para recuar
               os subtópicos.
             </li>
           </ul>
         </li>
-        <li>Clique em processar;</li>
-        <li>
+        <li className='list-decimal'>Clique em processar</li>
+        <li className='list-decimal'>
           Clique no botão copiar - canto superior direito - do campo{' '}
-          <strong>RESULTADO</strong>;
+          <strong>Resultado</strong>
         </li>
       </ol>
-    </>
+    </div>
   )
 }
