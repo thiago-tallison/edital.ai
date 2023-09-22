369b5801c503de4c842e51bb2546afdc4f2f7509
Thiago Tallison <thiagotallison@gmail.com>
2023-09-22 15:38:41 -0300

    feat: create layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+import MenuLink from '@/components/menulink'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
+import { Github } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +21,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang='pt-br'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='flex min-h-screen flex-col'>
+          <div className='flex items-center justify-between border-b px-6 py-3'>
+            <Link href='/'>
+              <h1 className='cursor-pointer select-none text-xl font-bold'>
+                edital.ai
+              </h1>
+            </Link>
+
+            <MenuLink />
+
+            <div>
+              <Button variant='outline'>
+                <Github className='mr-2 h-4 w-4' />
+                Github
+              </Button>
+            </div>
+          </div>
+          <div className='mx-auto mt-6 max-w-5xl'>
+            <div className='px-6'>{children}</div>
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,3 @@
-import MenuLink from '@/components/menulink'
-import { Button } from '@/components/ui/button'
-import { Github } from 'lucide-react'
-import Link from 'next/link'
-
 export default function Home() {
-  return (
-    <div className='flex min-h-screen flex-col'>
-      <div className='flex items-center justify-between border-b px-6 py-3'>
-        <Link href='/'>
-          <h1 className='cursor-pointer select-none text-xl font-bold'>
-            edital.ai
-          </h1>
-        </Link>
-
-        <MenuLink />
-
-        <div>
-          <Button variant='outline'>
-            <Github className='mr-2 h-4 w-4' />
-            Github
-          </Button>
-        </div>
-      </div>
-    </div>
-  )
+  return <h1>hello world</h1>
 }
