9e45e16bf394868f54548ddf36a2adf1107d3726
Thiago Tallison <thiagotallison@gmail.com>
2023-09-22 14:36:10 -0300

    feat: add header to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+import MenuLink from "@/components/menulink";
 import { Button } from "@/components/ui/button";
+import { Github } from "lucide-react";
 
 export default function Home() {
   return (
-    <>
-      <Button>Teste</Button>
-      <h1>Hello world</h1>
-    </>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="font-bold cursor-pointer select-none text-xl">
+          edital.ai
+        </h1>
+
+        <MenuLink />
+
+        <div>
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
